3749d8e1a0a742070edeedafeccc566b25e6baf8
Ruan Blotta <ruanblotta@gmail.com>
2024-04-23 20:06:33 -0300

    API Mercado Livre ok

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const URL = "https://api.mercadolibre.com/sites/MLB/search?q=notebooks";
 
-console.log(URL);
+fetch(URL)
+    .then(response =>  response.json())
+    .then(data => {
+
+        const productListDiv = document.getElementById('product-list');
+        data.results.forEach(product => {
+            const productTitle = document.createElement('li')
+            productTitle.textContent = product.title;
+            productListDiv.appendChild(productTitle);
+        });
+    })
+    .catch(error => console.error("Erro: ", erro));
+
